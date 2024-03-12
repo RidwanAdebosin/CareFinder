505d23b8bb7432f9ea0f38ef628d97fa4e806b48
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-03-12 18:57:02 +0100

    workinng on the firebase

--- index.tsx
+++ index.tsx
@@ -1,4 +1,7 @@
 import {initializeApp} from "firebase/app"
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBL0J48nwu2mnL9HNlYg4k0kgwZrNgSc2k",
@@ -9,4 +12,17 @@ const firebaseConfig = {
     appId: "1:953909968248:web:7acd4bc29e10093ba95c8d"
   };
 
+  //init firebase app
   initializeApp(firebaseConfig)
+
+  //init services
+const db = getFirestore()
+
+  //collection sef
+  const colRef = collection(db, 'names')
+
+  //get collection data
+  getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs)
+  })
