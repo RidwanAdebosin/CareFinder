76d45dfa2c554411eb90b9b21f72fb71a16e4695
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-03-12 10:05:11 +0100

    drag and drop form added

--- src/Pages/AddHospitals/AddHospitals.tsx
+++ src/Pages/AddHospitals/AddHospitals.tsx
@@ -1,33 +1,53 @@
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 import "./AddHospitals.css"
+import { FaPen } from "react-icons/fa";
 
 function AddHospitals(){
 return(
     <>
     <Navigation/>
     <div className="add-hospital-container">
+        <header>
+
+        <h2>List Hospital</h2>
+        <p>Use the markdown to fill required sections before submitting. As soon as we approve your hospital would appear in serach result</p>
+        </header>
+    <div className="add-hospital-wrapper">
     <main>
-        <p>Edit text</p>
-        <span>
-            <button className="btn">Save as draft</button>
-            <button className="btn">Post to public</button>
+        <p className="edit-text">Edit text <span><FaPen/></span></p>
+        <span className="add-hospital-btn">
+            <button className="add-btn">Save as draft</button>
+            <button className="add-btn">Post to public</button>
         </span>
     </main>
-    <div className="add-hospital-wrapper">
         <div className="add-hospital-details">
-            <form>
+            <form className="add-hospital-form" action="">
+                <p className="full-width">
                 <label>Name of hospital</label>
-                <input type="text" placeholder="..your hospital name here"/>
-                <label>Phone Number</label>
-                <input type="text" placeholder="..your hospital name here"/>
-                <label>Company email</label>
-                <input type="text" placeholder="..your hospital name here"/>
-                <textarea></textarea>
-            </form>
-        </div>
-        <div className="add-hospital-image">
+                <input type="text" placeholder="Type here"/>
+                </p>
+                
+                <p>
+                <label className="number-label">Phone Number</label>
+                <input type="number" placeholder="Type here" />
+                </p>
 
+                <p>
+                <label className="email-label">Company email</label>
+                <input type="email" placeholder="Type here" />
+                </p>
+                                
+
+                <p className="full-width">
+                    <label>Write a short bio</label>
+                    <textarea name="" id="" placeholder="write a short bio about your facility here"></textarea>
+                    
+                </p>
+            </form>
+        <div className="add-hospital-drop-zone">
+        <p>Drag and drop file here or <a>Browse files</a></p>
+        </div>
         </div>
     </div>
     </div>
