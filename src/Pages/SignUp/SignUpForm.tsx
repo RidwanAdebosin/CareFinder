81cde85e87a87edf328f6e0748e8822f0d7d7e43
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-03-15 10:43:41 +0100

    Form Authentication debugged

--- src/Pages/SignUp/SignUpForm.tsx
+++ src/Pages/SignUp/SignUpForm.tsx
@@ -4,8 +4,7 @@ import "./SignUp.css";
 import CareFinderLogo from ".//careFinderLogo.png"
 import { signup, useAuth, logout, login, } from "../../config";
 import { FormEvent, useRef, useState } from "react";
-import AddHospitals from "../AddHospitals/AddHospitals";
-// import AddHospitals from "../AddHospitals/AddHospitals";
+
 
 
 
@@ -30,25 +29,25 @@ const currentUser = useAuth();
 setLoading(false);
   }
 
-//   async function handlelogout(){
-//     setLoading(true);
-//   try {
-//   await logout();
-//   }catch{
-//     alert("Error!");
-//   }
-//   setLoading(false);
-// }
+  async function handleLogout(){
+    setLoading(true);
+  try {
+  await logout();
+  }catch{
+    alert("Error!");
+  }
+  setLoading(false);
+}
 
-// async function handlelogin(){
-//   setLoading(true);
-//   try {
-//     await login(emailRef.current.value, passwordRef.current.value);
-//   } catch {
-//     alert("Welcome {currentUser.email}")
-//   }
-//   setLoading(false);
-// }
+async function handleLogin(){
+  setLoading(true);
+  try {
+    await login(emailRef.current.value, passwordRef.current.value);
+  } catch {
+    alert("Welcome {currentUser.email}")
+  }
+  setLoading(false);
+}
   
  
 
@@ -61,10 +60,9 @@ setLoading(false);
           className="signup-carefinder-logo"
         />
       </div>
- Currently logged in as: {currentUser?.email}
- {/* <AddHospitals/> */}
-      {/* {!currentUser &&  */}
-      <>
+ {/* Currently logged in as: {currentUser?.email} */}
+
+      
       <div className="signup-form">
         <h1>Create Account</h1>
         <p>Sign up to get search for hospitals near you super fast!</p>
@@ -88,9 +86,10 @@ setLoading(false);
         
         <button className="signup-btn" disabled={loading || currentUser} onClick={handleSignup}>SignUp</button>
         
-        {/* <button className="signup-btn" disabled={loading || currentUser}  onClick={handlelogin}>Log In</button> */}
+        <button className="signup-btn" disabled={loading || !currentUser} onClick={handleLogout}>Log Out</button>
 
-        {/* <button className="signup-btn" disabled={loading || !currentUser} onClick={handlelogout}>Log Out</button> */}
+        <button className="signup-btn" disabled={loading || currentUser}  onClick={handleLogin}>Log In</button>
+
       
         <p>Or sign up with</p>
         <span className="signup-socials">
@@ -99,16 +98,7 @@ setLoading(false);
           <FaApple />
         </span>
       </div>
-      </>
-      {/* } */}
-
-  
-{/* 
-      { !currentUser && 
-      <>
-      <button className="signup-btn" disabled={loading || !currentUser} onClick={handlelogout}>Log Out</button>
-      </>} */}
-      
+       
     </div>
   );
 }
@@ -123,130 +113,3 @@ export default SignUpForm;
 
 
 
-
-// import { FaFacebook, FaGoogle, FaApple } from "react-icons/fa";
-// import "./SignUp.css";
-// // import { redirect } from "react-router-dom";
-// import CareFinderLogo from ".//careFinderLogo.png"
-// import { signup, useAuth, logout, login, signIn, auth } from "../../config";
-// import { FormEvent, useRef, useState } from "react";
-// // import AddHospitals from "../AddHospitals/AddHospitals";
-
-
-
-// function SignUpForm() {
-// const [loading, setLoading] =useState(false);
-// const currentUser = useAuth();
-
-//   const emailRef = useRef<HTMLInputElement>(null);
-//   const passwordRef = useRef<HTMLInputElement>(null);
-
-//   async function handleSignup(e: FormEvent){
-
-//     e.preventDefault();
-//   // if(!emailRef.current || !passwordRef.current){
-//   //   return;
-//   // } 
-//   // try{
-//   //   setLoading(true);
-//   //    signup(emailRef.current.value, passwordRef.current.value);
-    
-//   // } catch {
-//   //   alert("Error!")
-//   // }
-// setLoading(false);
-//   }
-
-//   async function handlelogout(){
-//     setLoading(true);
-//   try {
-//   await logout();
-//   }catch{
-//     alert("Error!");
-//   }
-//   setLoading(false);
-// }
-
-// async function handlelogin(){
-//   setLoading(true);
-//   try {
-  
-//     const User = await signIn(auth, emailRef.current.value, passwordRef.current.value);
-//    console.log(User)
-//   } catch {
-//     alert("Welcome {currentUser.email}")
-//   }
-//   setLoading(false);
-// }
-  
- 
-
-//   return (
-//     <div>
-//       <div className="signup-careFinder-logo">
-//         <img
-//           src={CareFinderLogo}
-//           alt="CareFinder"
-//           className="signup-carefinder-logo"
-//         />
-//       </div>
-//  {/* Currently logged in as: {currentUser?.email} */}
-//       {!currentUser && 
-//       <>
-//       <div className="signup-form">
-//         <h1>Create Account</h1>
-//         <p>Sign up to get search for hospitals near you super fast!</p>
-//         <div className="signup-input-container">
-
-//           <input
-//             ref={emailRef}
-//             type="email"
-//             placeholder="&#9993; 
-//               Email"
-//               className="signup-input"
-//               />
-
-//           <input
-//             ref={passwordRef}
-//             type="password"
-//             placeholder="&#42;&#42;&#42; password..."
-//             className="signup-input"
-//             />
-//         </div>
-        
-//         <button className="signup-btn" disabled={loading} onClick={handleSignup}>SignUp</button>
-        
-//         <button className="signup-btn" disabled={loading}  onClick={async() => {
-// await handlelogin()
-//         }}>Log In</button>
-
-//         <button className="signup-btn" disabled={loading || !currentUser} onClick={handlelogout}>Log Out</button>
-      
-//         <p>Or sign up with</p>
-//         <span className="signup-socials">
-//           <FaFacebook />
-//           <FaGoogle />
-//           <FaApple />
-//         </span>
-//       </div>
-//       </>
-//       }
-
-  
-
-//       { !currentUser && 
-//       <>
-//       <button className="signup-btn" disabled={loading || !currentUser} onClick={handlelogout}>Log Out</button>
-//       </>}
-      
-//     </div>
-//   );
-// }
-// export default SignUpForm;
-
-
-{/* <span className="go-back-to-homepage">
-  <Link to="/" className="go-back-to-homepage">
-    &larr;
-  </Link>
-</span> */}
