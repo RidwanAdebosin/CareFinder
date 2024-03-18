92e82a6b5ad26b3a373a491976489259f7fba94d
Ridwan Adebosin <adebosinadewale@gmail.com>
2024-03-18 23:18:10 +0100

    Working on the Private Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
-import SignUpForm from "./Pages/SignUp/SignUpForm";
 import HospitalLandingPage from "./Pages/HospitalLandPage/HospitalLandingPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HospitalList from "./Pages/HospitalList/HospitalList";
 import AddHospitals from "./Pages/AddHospitals/AddHospitals";
+import Login from "./Pages/SignUp/Login";
+import SignUpForm from "./Pages/SignUp/SignUpForm";
+import Profile from "./Pages/SignUp/UserProfile";
+import PrivateRoute from "./Pages/SignUp/PrivateRouting";
+
+
 
 
 function App() {
   return (
     <BrowserRouter>
-  
       <Routes>
-        <Route index element={<SignUpForm />} />
-        <Route path="landing-page" element={<LandingPage />} />
-        <Route
-          path="hospital-list"
-          element={<HospitalList/>}
-        />
-        <Route
-          path="hospital-list/:hospitalId"
-          element={<HospitalLandingPage />}
-        />
-        <Route
-        path="add-hospitals" element={<AddHospitals/>}/>
+      <Route path="/" element={<SignUpForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<PrivateRoute>
+          <Profile />
+          <Route path="landing-page" element={<LandingPage />} />
+          <Route path="hospital-list" element={<HospitalList />} />
+          <Route path="hospital-list/:hospitalId" element={<HospitalLandingPage />} />
+          <Route path="add-hospitals" element={<AddHospitals />} />
+          </PrivateRoute>} />
       </Routes>
-
     </BrowserRouter>
   );
 }
 
 export default App;
+
 
